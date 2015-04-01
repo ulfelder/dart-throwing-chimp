40b8c183c4d35e0ea59662690cf630452b165223
Jay Ulfelder <ulfelder@gmail.com>
2015-04-01 09:17:06 -0400

    Update f.yrmorack.R
    Adjust some start and end dates to match Polity.

--- coup.monthly/f.yrmorack.R
+++ coup.monthly/f.yrmorack.R
@@ -60,7 +60,7 @@ f.yrmorack <- function(startdate, enddate) {
      Congo <- kuntry("Congo-Brazzaville", "1960-08-15", enddate)
      IvoryCoast <- kuntry("Ivory Coast", "1960-08-07", enddate)
      Djibouti <- kuntry("Djibouti", "1977-06-27", enddate)
-     Egypt <- kuntry("Egypt", "1953-06-18", enddate)
+     Egypt <- kuntry("Egypt", "1922-03-01", enddate)
      EquatorialGuinea <- kuntry("Equatorial Guinea", "1968-10-12", enddate)
      Eritrea <- kuntry("Eritrea", "1993-05-24", enddate)
      Ethiopia <- kuntry("Ethiopia", "1000-01-01", enddate) # Really more like 1000 BC, but that's a hassle
@@ -83,18 +83,18 @@ f.yrmorack <- function(startdate, enddate) {
      Namibia <- kuntry("Namibia", "1990-03-21", enddate)
      Niger <- kuntry("Niger", "1960-08-03", enddate)
      Nigeria <- kuntry("Nigeria", "1960-10-01", enddate)
-     Rwanda <- kuntry("Rwanda", "1962-07-01", enddate)
+     Rwanda <- kuntry("Rwanda", "1961-07-01", enddate)
      SaoTome <- kuntry("Sao Tome and Principe", "1975-07-12", enddate)
-     Senegal <- kuntry("Senegal", "1960-08-20", enddate)
+     Senegal <- kuntry("Senegal", "1960-04-04", enddate) # Per Polity
      SierraLeone <- kuntry("Sierra Leone", "1961-04-27", enddate)
      Somalia <- kuntry("Somalia", "1960-07-01", enddate)
-     SouthAfrica <- kuntry("South Africa", "1931-12-11", enddate)
+     SouthAfrica <- kuntry("South Africa", "1910-05-31", enddate) # Per Polity
      SouthSudan <- kuntry("South Sudan", "2011-07-09", enddate)
      Sudan <- kuntry("Sudan", "1956-01-01", enddate)
      Swaziland <- kuntry("Swaziland", "1968-09-06", enddate)
      Tanzania <- kuntry("Tanzania", "1961-12-09", enddate)
      Togo <- kuntry("Togo", "1960-04-27", enddate)
-     Tunisia <- kuntry("Tunisia", "1956-03-20", enddate)
+     Tunisia <- kuntry("Tunisia", "1959-06-01", enddate) # Per Polity
      Uganda <- kuntry("Uganda", "1962-10-09", enddate)
      Zambia <- kuntry("Zambia", "1964-10-24", enddate)
      Zimbabwe <- kuntry("Zimbabwe", "1980-04-18", enddate)
@@ -111,29 +111,29 @@ f.yrmorack <- function(startdate, enddate) {
 
      Argentina <- kuntry("Argentina", "1816-07-09", enddate)
      Bolivia <- kuntry("Bolivia", "1825-08-06", enddate)
-     Brazil <- kuntry("Brazil", "1825-08-29", enddate)
+     Brazil <- kuntry("Brazil", "1824-03-25", enddate) # Per Polity
      Canada <- kuntry("Canada", "1867-07-01", enddate)
-     Chile <- kuntry("Chile", "1844-04-25", enddate)
+     Chile <- kuntry("Chile", "1818-02-12", enddate) # Per Polity
      Colombia <- kuntry("Colombia", "1819-08-07", enddate)
      CostaRica <- kuntry("Costa Rica", "1821-09-15", enddate)
-     Cuba <- kuntry("Cuba", "1898-12-10", enddate)
+     Cuba <- kuntry("Cuba", "1902-05-20", enddate) # Per Polity
      DominicanRepublic <- kuntry("Dominican Republic", "1865-03-03", enddate)
      Ecuador <- kuntry("Ecuador", "1830-05-13", enddate)
-     ElSalvador <- kuntry("El Salvador", "1841-01-01", enddate)
+     ElSalvador <- kuntry("El Salvador", "1841-02-16", enddate)
      Guatemala <- kuntry("Guatemala", "1839-04-17", enddate)
      Guyana <- kuntry("Guyana", "1966-05-26", enddate)
      Haiti <- kuntry("Haiti", "1804-01-01", enddate)
      Honduras <- kuntry("Honduras", "1838-10-26", enddate)
-     Jamaica <- kuntry("Jamaica", "1962-08-06", enddate)
+     Jamaica <- kuntry("Jamaica", "1959-07-04", enddate) # Per Polity
      Mexico <- kuntry("Mexico", "1821-08-24", enddate)
-     Nicaragua <- kuntry("Nicaragua", "1838-11-05", enddate)
+     Nicaragua <- kuntry("Nicaragua", "1838-04-30", enddate) # Per Polity
      Panama <- kuntry("Panama", "1903-11-03", enddate)
      Paraguay <- kuntry("Paraguay", "1811-05-14", enddate)
      Peru <- kuntry("Peru", "1879-01-01", enddate)
      Trinidad <- kuntry("Trinidad and Tobago", "1962-08-31", enddate)
      USA <- kuntry("United States", "1783-09-03", enddate)
-     Uruguay <- kuntry("Uruguay", "1828-08-27", enddate)
-     Venezuela <- kuntry("Venezuela", "1830-01-13", enddate)
+     Uruguay <- kuntry("Uruguay", "1830-05-26", enddate) # Per Polity
+     Venezuela <- kuntry("Venezuela", "1830-09-22", enddate) # Per Polity
 
      americas <- rbind(Argentina, Bolivia, Brazil, Canada, Chile,
           Colombia, CostaRica, Cuba, DominicanRepublic, Ecuador, ElSalvador,
@@ -144,35 +144,35 @@ f.yrmorack <- function(startdate, enddate) {
 
      Afghanistan <- kuntry("Afghanistan", "1747-01-01", enddate)
      Bahrain <- kuntry("Bahrain", "1971-08-15", enddate)
-     Bangladesh <- kuntry("Bangladesh", "1971-03-26", enddate)
-     Bhutan <- kuntry("Bhutan", "1885-01-01", enddate)
+     Bangladesh <- kuntry("Bangladesh", "1972-04-18", enddate) # Per Polity
+     Bhutan <- kuntry("Bhutan", "1907-12-17", enddate) # Per Polity
      Cambodia <- kuntry("Cambodia", "1953-09-09", enddate)
      China <- kuntry("China", "1000-01-01", enddate) # Earlier, I know; this is a convenience.
      India <- kuntry("India", "1947-08-15", enddate)
-     Indonesia <- kuntry("Indonesia", "1949-12-27", enddate)
+     Indonesia <- kuntry("Indonesia", "1945-08-17", enddate)
      Iran <- kuntry("Iran", "1000-01-01", enddate) # Placeholder
-     Iraq <- kuntry("Iraq", "1932-10-03", enddate)
+     Iraq <- kuntry("Iraq", "1924-07-10", enddate)
      Israel <- kuntry("Israel", "1948-05-14", enddate)
      Japan <- kuntry("Japan", "1000-01-01", enddate) # Yeah, yeah, yeah...
      Jordan <- kuntry("Jordan", "1946-05-26", enddate)
      Kuwait <- kuntry("Kuwait", "1961-06-19", enddate)
-     Laos <- kuntry("Laos", "1949-07-19", enddate)
+     Laos <- kuntry("Laos", "1953-10-22", enddate)
      Lebanon <- kuntry("Lebanon", "1943-11-22", enddate)
      Malaysia <- kuntry("Malaysia", "1957-08-31", enddate)
      Mongolia <- kuntry("Mongolia", "1911-12-29", enddate)
      Myanmar <- kuntry("Myanmar", "1948-01-04", enddate)
      Nepal <- kuntry("Nepal", "1768-12-21", enddate)
-     NorthKorea <- kuntry("North Korea", "1948-09-09", enddate)
+     NorthKorea <- kuntry("North Korea", "1948-05-01", enddate)
      Oman <- kuntry("Oman", "1650-01-26", enddate)
      Pakistan <- kuntry("Pakistan", "1947-08-14", enddate)
-     Philippines <- kuntry("Philippines", "1898-06-12", enddate)
+     Philippines <- kuntry("Philippines", "1935-11-24", enddate) # Per Polity
      Qatar <- kuntry("Qatar", "1971-09-03", enddate)
-     SaudiArabia <- kuntry("Saudi Arabia", "1927-05-20", enddate)
+     SaudiArabia <- kuntry("Saudi Arabia", "1926-01-16", enddate) # Per Polity
      Singapore <- kuntry("Singapore", "1965-08-09", enddate)
      SouthKorea <- kuntry("South Korea", "1948-08-15", enddate)
-     SriLanka <- kuntry("Sri Lanka", "1972-05-22", enddate)
-     Syria <- kuntry("Syria", "1946-04-17", enddate)
-     Taiwan <- kuntry("Taiwan", "1949-01-01", enddate)
+     SriLanka <- kuntry("Sri Lanka", "1948-02-04", enddate)
+     Syria <- kuntry("Syria", "1944-07-01", enddate) # Per Polity
+     Taiwan <- kuntry("Taiwan", "1949-10-01", enddate) # Per Polity
      Thailand <- kuntry("Thailand", "1000-01-01", enddate) # Again...
      TimorLeste <- kuntry("Timor Leste", "2002-05-20", enddate)
      UAE <- kuntry("United Arab Emirates", "1971-12-02", enddate)
@@ -191,49 +191,49 @@ f.yrmorack <- function(startdate, enddate) {
      Austria <- kuntry("Austria", "1918-11-12", enddate)
      Belarus <- kuntry("Belarus", "1991-08-25", enddate)
      Belgium <- kuntry("Belgium", "1830-10-04", enddate)
-     Bosnia <- kuntry("Bosnia and Herzegovina", "1992-03-03", enddate)
-     Bulgaria <- kuntry("Bulgaria", "1908-09-22", enddate)
-     Croatia <- kuntry("Croatia", "1991-10-08", enddate)
+     Bosnia <- kuntry("Bosnia and Herzegovina", "1992-04-05", enddate) # Por Polity
+     Bulgaria <- kuntry("Bulgaria", "1879-04-29", enddate) # Per Polity
+     Croatia <- kuntry("Croatia", "1991-06-25", enddate) # Per Polity
      Cyprus <- kuntry("Cyprus", "1960-08-16", enddate)
      CzechRepublic <- kuntry("Czech Republic", "1993-01-01", enddate)
      Denmark <- kuntry("Denmark", "1000-01-01", enddate)
-     Estonia <- kuntry("Estonia", "1991-08-25", enddate)
+     Estonia <- kuntry("Estonia", "1991-09-06", enddate)
      Finland <- kuntry("Finland", "1918-01-03", enddate)
      France <- kuntry("France", "1000-01-01", enddate)
      Germany <- kuntry("Germany", "1990-10-03", enddate)
-     Greece <- kuntry("Greece", "1832-03-04", enddate)
-     Hungary <- kuntry("Hungary", "1849-01-01", enddate)
-     Ireland <- kuntry("Ireland", "1922-12-06", enddate)
+     Greece <- kuntry("Greece", "1827-05-17", enddate)
+     Hungary <- kuntry("Hungary", "1867-05-29", enddate) # Per Polity
+     Ireland <- kuntry("Ireland", "1921-12-06", enddate)
      Italy <- kuntry("Italy", "1861-03-17", enddate)
      Kosovo <- kuntry("Kosovo", "2008-02-17", enddate)
-     Latvia <- kuntry("Latvia", "1991-08-25", enddate)
-     Lithuania <- kuntry("Lithuania", "1991-08-25", enddate)
+     Latvia <- kuntry("Latvia", "1991-09-06", enddate)
+     Lithuania <- kuntry("Lithuania", "1991-09-06", enddate)
      Macedonia <- kuntry("Macedonia", "1991-09-08", enddate)
-     Moldova <- kuntry("Moldova", "1991-08-25", enddate)
+     Moldova <- kuntry("Moldova", "1991-08-27", enddate)
      Montenegro <- kuntry("Montenegro", "2006-06-03", enddate)
      Netherlands <- kuntry("Netherlands", "1648-05-15", enddate)
-     Norway <- kuntry("Norway", "1905-06-07", enddate)
+     Norway <- kuntry("Norway", "1814-05-17", enddate) # Per Polity
      Poland <- kuntry("Poland", "1918-11-11", enddate)
      Portugal <- kuntry("Portugal", "1143-10-05", enddate)
-     Romania <- kuntry("Romania", "1878-07-13", enddate)
-     Serbia <- kuntry("Serbia", "2006-06-08", enddate)
+     Romania <- kuntry("Romania", "1859-01-24", enddate) # Per Polity
+     Serbia <- kuntry("Serbia", "2006-06-03", enddate)
      Slovakia <- kuntry("Slovakia", "1993-01-01", enddate)
      Slovenia <- kuntry("Slovenia", "1991-06-25", enddate)
      Spain <- kuntry("Spain", "1512-01-01", enddate)
      Sweden <- kuntry("Sweden", "1523-06-06", enddate)
-     Switzerland <- kuntry("Switzerland", "1291-01-01", enddate)
+     Switzerland <- kuntry("Switzerland", "1848-09-12", enddate) # Per Polity
      Turkey <- kuntry("Turkey", "1923-10-29", enddate)
-     Ukraine <- kuntry("Ukraine", "1991-08-25", enddate)
+     Ukraine <- kuntry("Ukraine", "1991-12-01", enddate)
      UnitedKingdom <- kuntry("United Kingdom", "1707-05-01", enddate)
-     Armenia <- kuntry("Armenia", "1991-08-25", enddate)
-     Azerbaijan <- kuntry("Azerbaijan", "1991-08-25", enddate)
-     Georgia <- kuntry("Georgia", "1991-08-25", enddate)
-     Kazakhstan <- kuntry("Kazakhstan", "1991-08-25", enddate)
-     Kyrgyzstan <- kuntry("Kyrgyzstan", "1991-08-25", enddate)
-     Russia <- kuntry("Russia", "1991-08-25", enddate)
-     Tajikistan <- kuntry("Tajikistan", "1991-08-25", enddate)
-     Turkmenistan <- kuntry("Turkmenistan", "1991-08-25", enddate)
-     Uzbekistan <- kuntry("Uzbekistan", "1991-08-25", enddate)
+     Armenia <- kuntry("Armenia", "1991-09-22", enddate)
+     Azerbaijan <- kuntry("Azerbaijan", "1991-08-30", enddate)
+     Georgia <- kuntry("Georgia", "1991-04-09", enddate)
+     Kazakhstan <- kuntry("Kazakhstan", "1991-12-16", enddate)
+     Kyrgyzstan <- kuntry("Kyrgyzstan", "1991-08-31", enddate)
+     Russia <- kuntry("Russia", "1991-12-25", enddate)
+     Tajikistan <- kuntry("Tajikistan", "1991-09-09", enddate)
+     Turkmenistan <- kuntry("Turkmenistan", "1991-10-27", enddate)
+     Uzbekistan <- kuntry("Uzbekistan", "1991-08-31", enddate)
 
      europe <- rbind(Albania, Austria, Belgium, Bosnia, Bulgaria, Croatia, Cyprus,
           CzechRepublic, Denmark, Finland, France, Germany, Greece, Hungary,
@@ -249,7 +249,7 @@ f.yrmorack <- function(startdate, enddate) {
 
      Australia <- kuntry("Australia", "1901-01-01", enddate)
      Fiji <- kuntry("Fiji", "1970-10-10", enddate)
-     NewZealand <- kuntry("New Zealand", "1907-09-26", enddate)
+     NewZealand <- kuntry("New Zealand", "1857-07-01", enddate) # Per Polity
      PapuaNewGuinea <- kuntry("Papua New Guinea", "1975-09-16", enddate)
      SolomonIslands <- kuntry("Solomon Islands", "1978-07-07", enddate)
 
@@ -257,17 +257,17 @@ f.yrmorack <- function(startdate, enddate) {
 
      # DEFUNCT
 
-     Czechoslovakia <- kuntry("Czechoslovakia", "1918-11-11", "1992-12-31")
-     Yugoslavia <- kuntry("Yugoslavia", "1918-11-11", "1992-01-15")
-     FedRepYugoslavia <- kuntry("Federal Republic of Yugoslavia", "1992-01-15", "2003-02-04")
-     SerbiaMontenegro <- kuntry("Serbia and Montenegro", "2003-02-04", "2006-06-08")
-     WestGermany <- kuntry("West Germany", "1945-08-02", "1990-10-03")
-     EastGermany <- kuntry("East Germany", "1945-08-02", "1990-10-03")
-     USSR <- kuntry("Soviet Union", "1922-12-28", "1991-08-25")
+     Czechoslovakia <- kuntry("Czechoslovakia", "1918-10-28", "1992-12-31")
+     Yugoslavia <- kuntry("Yugoslavia", "1921-01-01", "1991-07-01") # Per Polity
+     FedRepYugoslavia <- kuntry("Federal Republic of Yugoslavia", "1992-01-15", "2003-03-11")
+     SerbiaMontenegro <- kuntry("Serbia and Montenegro", "2003-03-11", "2006-06-03")
+     WestGermany <- kuntry("West Germany", "1945-05-08", "1990-10-03") # Per Polity
+     EastGermany <- kuntry("East Germany", "1945-05-08", "1990-10-03") # Per Polity
+     USSR <- kuntry("Soviet Union", "1922-12-28", "1991-12-26")
      NorthYemen <- kuntry("North Yemen", "1918-11-01", "1990-05-22")
      SouthYemen <- kuntry("South Yemen", "1967-11-30", "1990-05-22")
-     NorthVietnam <- kuntry("North Vietnam", "1954-07-21", "1976-07-02")
-     SouthVietnam <- kuntry("South Vietnam", "1954-07-21", "1976-07-02")
+     NorthVietnam <- kuntry("North Vietnam", "1954-05-07", "1976-07-02")
+     SouthVietnam <- kuntry("South Vietnam", "1955-10-26", "1976-07-02") # Per Polity
 
      defunct <- rbind(Czechoslovakia, Yugoslavia, FedRepYugoslavia, SerbiaMontenegro,
           WestGermany, EastGermany, USSR, NorthYemen, SouthYemen,
