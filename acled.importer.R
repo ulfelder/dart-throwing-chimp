545924f02ae3e5d56d2a2f4c32a2a85bd365f9a5
Jay Ulfelder <ulfelder@gmail.com>
2015-07-19 10:04:31 -0400

    Drop future months, add country names and plots
    Revised country-month aggregation process to omit all current and future country-months; added creation of country name field using 'countrycode'; and added plotting examples.

--- acled.importer.R
+++ acled.importer.R
@@ -1,11 +1,13 @@
 # This script will download, unzip, prep, and merge ACLED's Version 5 historical data (1997-2014) and its realtime
 # data. The downside is that it will need to be modified as the realtime data are updated, or if the url or filename for
 # the historical data changes. The upside is that those changes only need to be made in the "File info" section that
-# starts on line 19. Unless the basic structure of the files or their variable names change, the rest should keep working.
+# starts on line 21. Unless the basic structure of the files or their variable names change, the rest should keep working.
 
 # Load required packages, which are only used in country-month aggregation stage
 library(dplyr)
 library(tidyr)
+library(countrycode)
+library(ggplot2)
 
 # Function to get zip file and extract csv using vector of two string objects and returning data frame
 getfile <- function(vector) {
@@ -45,13 +47,32 @@ names(ACLED) <- tolower(names(ACLED)) # Convert var names in merged file to lowe
 # Inspect the result to make sure it's worked as expected
 str(ACLED)
 
-# Create new data frame with country-month counts of each event type and a count all battles of any type
+# Get country-month counts of each event type and add column counting all battles of any type
 ACLED.cm <- ACLED %>%
-  mutate(event_type = make.names(tolower(event_type))) %>% # Change event type labels for upcoming use as var names, and to deal with "Remote Violence", "Remote violence"
+  mutate(event_type = make.names(tolower(event_type))) %>% # Change event type labels for use as proper var names, and to deal with "Remote Violence", "Remote violence"
   mutate(month = as.numeric(substr(event_date, 4, 5))) %>%  # Create month var to use in grouping
-  group_by(gwno, year, month, event_type) %>%  # Define groupings and re-order data accordingly
+  group_by(gwno, year, month, event_type) %>%  # Define groupings from highest to lowest level; data are automatically ordered accordingly
   tally(.) %>%  # Get counts of records in each group (i.e., each country/year/month/type subset)
-  spread(., key = event_type, value = n, fill = 0) %>% # Make data wide by spreading event types into columns with tidyr's spread() function
+  spread(., key = event_type, value = n, fill = 0) %>% # Make data wide by spreading event types into columns
   left_join(expand(., gwno, year, month), .) %>% # Expand data frame to cover all possible country-months by left-joining tallies to complete series created with expand() from tidyr
   replace(is.na(.), 0) %>%  # Replace all NAs created by that last step with 0s
-  mutate(., battles = rowSums(select(., grep("battle", names(.))))) # Create var summing counts of all variables with "battle" in their names
+  mutate(., battles = rowSums(select(., contains("battle")))) %>% # Create var summing counts of all battle types
+  filter(., year < as.numeric(substr(Sys.Date(), 1, 4)) | (year == as.numeric(substr(Sys.Date(), 1, 4)) & month < as.numeric(substr(Sys.Date(), 6, 7)))) %>% # Drop rows for months that haven't happened yet
+  mutate(., country = countrycode(gwno, "cown", "country.name", warn = FALSE)) # Use 'countrycode' to add country names based on COW numeric codes
+  
+# Some examples of time-series plots
+
+# Montly counts of events involving violence against civilians in Burundi
+ACLED.cm %>%
+  filter(., country == "Burundi") %>%
+  mutate(., yearmo = as.Date(paste(year, ifelse(month < 10, paste0("0", month), month), "01", sep="-"))) %>%
+  qplot(data = ., x = yearmo, y = violence.against.civilians, geom = "line", xlab="Month", ylab="Event count") + ggtitle("Violence against civilians in Burundi") %>%
+  print
+
+# Monthly counts of battles by country (small multiples)
+ACLED.cm %>%
+  mutate(., yearmo = as.Date(paste(year, ifelse(month < 10, paste0("0", month), month), "01", sep="-"))) %>%
+  qplot(data = ., x = yearmo, y = battles, facets = ~country, geom = "line", xlab="Month", ylab="Event count")
+  print
+# If you'd rather save that plot to your hard drive, replace the 'print' line with one like this:
+# ggsave("ACLED.battles.ts.png", path = "[path to directory in which to save it]", width = 6, height = 8, units="in")
