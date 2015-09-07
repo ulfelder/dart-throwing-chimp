53371816836c85f7c39f3ed25cfe2fb5e07b47b4
Jay Ulfelder <ulfelder@gmail.com>
2015-09-07 12:48:57 -0400

    Update legostarwars.R
    Added some plots for full data set

--- legostarwars.R
+++ legostarwars.R
@@ -26,3 +26,16 @@ lego.grid
 png("legostarwars.png", width=6, height=6, unit="in", res=300)
 lego.grid
 dev.off()
+
+# Exploring the full data set a few ways
+group_by(Lego, Year) %>%
+  summarise(theme.count = length(unique(Theme))) %>%
+  ggplot(., aes(x=Year, y=theme.count)) + geom_line() + theme_bw() + ylab("Number of themes represented in new sets")
+group_by(Lego, Year) %>%
+  summarise(sets.released = n()) %>%
+  ggplot(., aes(x=Year, y=sets.released)) + geom_line() + theme_bw() + ylab("Number of new sets released")
+group_by(Lego, Year) %>%
+  summarise(theme.count = length(unique(Theme)), sets.released = n()) %>%
+  melt(., id.vars="Year") %>%
+  ggplot(., aes(x=Year, y=value, color=variable)) + geom_line()
+ggplot(tally(group_by(Lego, Year)), aes(x=Year, y=n)) + geom_line() + theme_bw() + ylab("Number of sets released")
