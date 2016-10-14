dbd0d7e767efbe48fbeb1f91237e6810041ad1f2
Jay Ulfelder <ulfelder@gmail.com>
2016-10-14 13:49:57 -0400

    Update fetchPWT.R
    Fixed error for versions named *.0 (e.g., 9.0)

--- fetchPWT.R
+++ fetchPWT.R
@@ -1,13 +1,21 @@
-# Function to read Penn World Tables into R using version number only
-# For example, for Version 8.1, run fetchPWT(8.1)
-# Only tested on Windows machine with versio 8.1, in which data are on sheet 3.
+# Function to read Penn World Tables into R using version number as string, e.g.
+# PWT <- fetchPWT("9.0")
+# Only tested on Windows 10 machine; last tested 2016-10-14
 
 fetchPWT <- function(version) {
-    require(readxl)
-    vs <- sub("\\.", "", version)
-    url <- paste0("http://www.rug.nl/research/ggdc/data/pwt/v", vs, "/pwt", vs, ".xlsx")
-    temp <- paste0(tempfile(), ".xlsx")
-    download.file(url, destfile = temp, mode = "wb")
-    pwt <- read_excel(path = temp, sheet = 3)
-    return(pwt)
+    
+  require(readxl)
+    
+  vs <- sub("\\.", "", version)
+    
+  url <- paste0("http://www.rug.nl/research/ggdc/data/pwt/v", vs, "/pwt", vs, ".xlsx")
+  
+  temp <- paste0(tempfile(), ".xlsx")
+  
+  download.file(url, destfile = temp, mode = "wb")
+  
+  PWT <- read_excel(path = temp, sheet = 3)
+  
+  return(PWT)
+
 }
