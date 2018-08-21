6bd8aaf6d5e859bd25df3a2b1a5ace2f2e7ee40b
Jay Ulfelder <ulfelder@gmail.com>
2018-08-21 12:15:57 -0400

    Update fetchFSI.R
    Heavily revised to work with FFP's revamped data download page

--- fetchFSI.R
+++ fetchFSI.R
@@ -1,93 +1,48 @@
-# This script gets the Fund for Peace's Failed States Index data files from the web and makes a country-year
-# table from them
+# This script gets the Fund for Peace's Fragile States Index data files from the web and makes
+# a country-year table from them
 
-library(dplyr)
-library(stringr)
+library(tidyverse)
+library(rvest)
 library(readxl)
-library(countrycode)
+library(lubridate)
 
-url.archive <- "http://fsi.fundforpeace.org/library/fragilestatesindex-2006to2014.xlsx"
-url.2015 <- "http://fsi.fundforpeace.org/library/fragilestatesindex-2015.xlsx"
-url.2016 <- "http://fsi.fundforpeace.org/library/fragilestatesindex-2016.xlsx"
+options(stringsAsFactors = FALSE)
 
-# get vector of years covered by archive sheet from file name to use for ingesting it
-archive.years <- as.numeric(str_extract_all(url.archive, "\\(?[0-9]+\\)?")[[1]])
-archive.years <- seq(archive.years[1], archive.years[2])
+# scrape the urls for the excel files from the FFP download page
+fsi_urls <- read_html("http://fundforpeace.org/fsi/excel/") %>%
+    # found this by inspecting the html in Chrome
+    html_nodes("td") %>%
+    # grab the urls from those nodes, which are basically long strings; I tried http_attr("html")
+    # and a couple of variations, but it didn't work
+    lapply(., function(x) str_extract(as.character(x), "http://fundforpeace.org/.{1,}.xlsx")) %>%
+    # each url is repeated, so get rid of the duplicates
+    unique(.) %>%
+    # get rid of the remaining NA
+    .[!is.na(.)] %>%
+    # one of the duplicates was a jackelope for some reason, so we need this to get rid of it
+    .[!grepl("a href", .)]
 
-# vector of cleaner names to give to columns as ingested
-fsivars <- c("rank", "country", "fsi.total", "fsi.demographic.pressures", "fsi.refugees",
-             "fsi.group.grievance", "fsi.human.flight", "fsi.uneven.development", 
-             "fsi.poverty", "fsi.legitimacy", "fsi.services", "fsi.human.rights",
-             "fsi.security", "fsi.faction", "fsi.intervention")
-
-# archived years
-temp <- paste0(tempfile(), ".xlsx")
-
-download.file(url.archive, destfile = temp, mode = "wb")
-
-FSI.archive <- lapply(archive.years, function(i) {
-
-  DF <- read_excel(path = temp, sheet = match(i, archive.years))
-
-  names(DF) <- fsivars
-
-  DF$year <- i
-
-  # for some reason this was causing trouble, and it's easy to recreate, so...buh-bye
-  DF$rank <- NULL
-
-  return(DF)
-
+# download the excel files from those urls to tempfiles and read them into R as data tables
+fsi_list <- lapply(fsi_urls, function(x) {
+	
+	tmp <- tempfile()
+	
+	download.file(x, tmp)
+	
+	df <- read_excel(tmp)
+	
 })
 
-FSI.archive <- bind_rows(FSI.archive)
+# collapse that list of annual data tables into one big data frame
+fsi_db <- bind_rows(fsi_list)
 
-# standalone years
+# clean up the column names
+names(fsi_db) <- names(fsi_db) %>%
+    gsub("[A-Z]{1}[0-9]{1}: ", "", .) %>%
+    tolower(.) %>%
+    make.names(.)
 
-fetchFSI <- function(year) {
-
-  url <- sprintf("http://fsi.fundforpeace.org/library/fragilestatesindex-%s.xlsx", year)
-
-  temp <- paste0(tempfile(), ".xlsx")
-  
-  download.file(url, destfile = temp, mode = "wb")
-
-  FSI <- read_excel(path = temp)
-
-  unlink(temp)
-
-  names(FSI) <- fsivars
-
-  FSI$year <- year
-
-  FSI$rank <- NULL
-
-  return(FSI)
-
-}
-
-FSI.2015 <- fetchFSI(2015)
-FSI.2016 <- fetchFSI(2016)
-
-# put it all together
-FSI <- bind_rows(list(FSI.archive, FSI.2015, FSI.2016)) %>%
-  arrange(country, year) %>%
-  filter(country != "AVERAGE" & !is.na(country))
-
-# fix some country name typos before adding codes; got this with manual check
-# for rows where is.na(code) after running countrycode
-FSI$country[FSI$country == "Dijbouti"] <- "Djibouti"
-FSI$country[FSI$country == "Serbia and Montenegro"] <- "Serbia"
-FSI$country[FSI$country == "Trinadad"] <- "Trinidad and Tobago"
-
-# add country code, reorder columns, drop country name to avoid merging hassles, 
-# and recreate ranking variable
-FSI <- FSI %>%
-  mutate(code = countrycode::countrycode(country, "country.name", "iso3c")) %>%
-  select(-country) %>%
-  group_by(year) %>%
-  arrange(-fsi.total) %>%
-  mutate(rank = seq_along(fsi.total)) %>%
-  select(code, year, rank, everything()) %>%
-  ungroup() %>%
-  arrange(code, year)
+# convert the date field to a simple year and drop the messy rank column
+fsi_db <- fsi_db %>%
+    mutate(year = lubridate::year(year)) %>%
+    select(-rank)
